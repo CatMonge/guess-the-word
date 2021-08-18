5c93b5e0e3ad7ac97c89af936af0b9e270e8299f
catarina_m <catarina.monge@gmail.com>
2021-08-17 21:39:09 -0400

    Corrected variable names. Removed symbol from placeholder. Added functions for input validation and code for displaying guessed letters, revealing word, and winning game.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetter = document.querySelector(".guessed-letters");
+const letterGuess = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const guessedLetterInput = document.querySelector(".letter");
 const wordsInProgress = document.querySelector(".word-in-progress");
@@ -12,11 +12,13 @@ const guessedLetters = [];
 
 const placeholder = function (word){
 const placeholderLetters = [];
-for (const letters of word){
-console.log(letters);
+for (const letter of word){
+console.log(letter);
 placeholderLetters.push("●");
 }
-wordsInProgress.innerText = placeholderLetters.join("●");
+
+wordsInProgress.innerText = placeholderLetters.join("");
+
 };
 
 placeholder(word);
@@ -25,12 +27,14 @@ guessButton.addEventListener("click", function(e){
 e.preventDefault();
 message.innertext = "";
 const guess = guessedLetterInput.value;
-console.log(guess);
+//console.log(guess);
+
+const goodGuess = inputValidation(guess);
+if(goodGuess){
+    makeGuess(guess)
+}
 guessedLetterInput.value = "";
 
-const inputResults = inputValidation(guess);
-makeGuess(inputResults)
-//console.log (inputResults);
 });
 
 const inputValidation = function (input){
@@ -41,9 +45,9 @@ const inputValidation = function (input){
         message.innerText = "Please enter only 1 letter at a time";
     }else if (!input.match(acceptedLetter)){
         message.innerText = "Please enter a letter from A-Z";
+    } else{
+        return input;
     }
-    
-    return input;
 };
 
 const makeGuess = function(guess){
@@ -54,6 +58,43 @@ if (guessedLetters.includes(guess)){
 }else {
     guessedLetters.push(guess)
     console.log(guessedLetters);
+    showLetter();
+    updateWordInProgress(guessedLetters);
 }
-return guess;
+//return guess;
+};
+
+const showLetter = function (){
+   letterGuess.innerHTML = "";
+   for (const letter of guessedLetters){
+    const li = document.createElement("li");
+    li.innerText = letter;
+    letterGuess.append(li);
+   };
+
+};
+
+const updateWordInProgress = function(guessedLetters){
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    //console.log(wordArray);
+    const showWord = [];
+
+    for (const letter of wordArray){
+        if (guessedLetters.includes(letter)){
+            showWord.push(letter.toUpperCase());
+        } else {
+            showWord.push ("●");
+        }
+    }
+   wordsInProgress.innerText = showWord.join("");
+   wonGame();
+};
+
+const wonGame = function(){
+    //console.log(wordsInProgress.innerText)
+    if (word.toUpperCase() === wordsInProgress.innerText){
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+    } 
 };
